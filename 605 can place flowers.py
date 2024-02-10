e4e6f76dc0fb3e9db2288d4a32f11153b24a975d
Sajia09 <sajiaa09@gmail.com>
2024-02-11 01:12:02 +0600

    605 corrected

--- 605 can place flowers.py
+++ 605 can place flowers.py
@@ -1,13 +1,14 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
-        count = 0
-        i = 0
-        while i < len(flowerbed):
-            if flowerbed[i] == 0 and (i==0 or flowerbed[i-1]==0) and (flowerbed[i+1]==0 or i == len(flowerbed)-1):
+        if n == 0:
+            return True
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
                 flowerbed[i] = 1
-                count+=1
-            i+=1
-        return count>=n
+                n -= 1
+                if n == 0:
+                    return True
+        return False
 
 
 sol = Solution()
